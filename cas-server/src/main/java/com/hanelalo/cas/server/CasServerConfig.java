d4a3582f4463ad21e3a72b462bf408fcae01ad71
hanelalo <killerkingye@163.com>
2020-02-16 14:09:54 +0800

    change spring boot and cloud version

--- cas-server/src/main/java/com/hanelalo/cas/server/CasServerConfig.java
+++ cas-server/src/main/java/com/hanelalo/cas/server/CasServerConfig.java
@@ -2,8 +2,10 @@ package com.hanelalo.cas.server;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
+@Order(1000)
 @ComponentScan(basePackageClasses = CasServerConfig.class)
 public class CasServerConfig {
 
